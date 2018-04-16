535b9b7c84e5f8198dcd934426757f2c588748e8
Vin Ratford <vratford@gmail.com>
2018-04-16 16:31:45 -0700

    Cloned SeeFood, modified model to Food101

---
+++ SeeFood101/ViewController.swift
@@ -0,0 +1,111 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Vincent Ratford on 3/24/18.
+//  Copyright © 2018 Vincent Ratford. All rights reserved.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var firstguess: UITextField!
+    
+    let imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera // use .photolibrary with simulator and .camera with device
+        imagePicker.allowsEditing = true     // make true if you want to allow editing, ie: cropping image for classification
+        
+        
+        
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let userPickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+               imageView.image = userPickedImage
+            
+            guard let ciimage = CIImage(image: userPickedImage) else {
+                fatalError("Could not convert UIIamage to CIImage")
+            }
+        
+            detect(image: ciimage)
+        }
+        
+      imagePicker.dismiss(animated: true, completion: nil)
+        
+        
+    }
+    
+    func detect(image: CIImage) {
+        
+        guard let model = try? VNCoreMLModel(for: Food101().model) else {
+            fatalError("Loading CoreML Model Failed.")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to process image")
+            }
+            
+//            print(results)
+            
+            if let firstResult = results.first {
+            
+                switch firstResult.identifier {
+                case "hotdog, hot dog, red hot":
+                    DispatchQueue.main.async {
+                        self.navigationItem.title = "Hotdog!"
+                        self.navigationController?.navigationBar.barTintColor = UIColor.green
+                        self.navigationController?.navigationBar.isTranslucent = false
+                    }
+                case "orange":
+                    self.navigationItem.title = "Orange!"
+                case "lemon":
+                    self.navigationItem.title = "Lemon"
+                case "pasta":
+                        self.navigationItem.title = "Pasta!"
+                case "paper towel":
+                    self.navigationItem.title = "Paper Towel!"
+                    default:
+                        self.navigationItem.title = "My First Guess!"
+                    self.firstguess.text = "\(firstResult.identifier) @ \(round(firstResult.confidence*1000) / 10)%"
+                }
+ 
+                self.navigationController?.navigationBar.barTintColor = UIColor.white
+            }
+        
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        }
+        catch {
+            print(error)
+        }
+        
+
+    }
+    
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
+
+}
+
